e521f6f18f46479fe2ac9b6717030c26ac6a05de
Mehdi ESSALEHI <mehdi.essalehi@proton.me>
2022-10-23 03:38:53 +0100

    feat: Started auth implementation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotAcceptableException } from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  
+  constructor (private users: UsersService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.users.user({ email });
+    if (!user) {
+      throw new NotAcceptableException("Could not find user");
+    }
+    if (user && await bcrypt.compare(password, user.password)) {
+      return user;
+    }
+    return null;
+  }
 }
---
+++ src/auth/local.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from "passport-local";
+import { AuthService } from "./auth.service";
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor (private auth: AuthService) {
+    super({ usernameField: 'email' });
+  }
+
+  async validate(email: string, password: string) {
+    const user = await this.auth.validateUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
