974a51a7bec8c5f5f0fb39cd1fd699a1898ee4fb
Mehdi Essalehi <mehdi.essalehi@gmail.com>
2023-02-06 00:12:56 +0100

    fix: used enums instead of hardcoded strings

--- src/application/application.controller.ts
+++ src/application/application.controller.ts
@@ -1,8 +1,9 @@
 import { Body, Controller, Get, Param, Patch, Post } from "@nestjs/common";
+import { ApplicationStatus } from "@prisma/client";
 import { ApplicationService } from "./application.service";
 import { ApplicationDto } from "./dto/ApplicationDto";
 
-@Controller("applications")
+@Controller("admin/applications")
 export class ApplicationController {
   constructor(private applications: ApplicationService) {}
 
@@ -12,7 +13,16 @@ export class ApplicationController {
     return await this.applications.addApplication(fullname, email);
   }
 
+  @Get()
+  async getAllApplications() {
+    return await this.applications.getAllApplications();
   }
+
+  @Get("pending")
+  async getAllPendingApplications() {
+    return await this.applications.getAllPendingApplications();
+  }
+
   @Get(":application")
   async getApplication(@Param("application") applicationId: number) {
     return await this.applications.getApplication(applicationId);
@@ -20,7 +30,10 @@ export class ApplicationController {
 
   @Post(":application/accept")
   async acceptApplication(@Param("application") applicationId: number) {
-    await this.applications.respondToApplication(applicationId, "GRANTED");
+    await this.applications.respondToApplication(
+      applicationId,
+      ApplicationStatus.GRANTED
+    );
     return {
       message: "The application has been successfully accepted"
     };
@@ -28,7 +41,10 @@ export class ApplicationController {
 
   @Post(":application/reject")
   async rejectApplication(@Param("application") applicationId: number) {
-    await this.applications.respondToApplication(applicationId, "REJECTED");
+    await this.applications.respondToApplication(
+      applicationId,
+      ApplicationStatus.REJECTED
+    );
     return {
       message: "The application has been successfully rejected"
     };
