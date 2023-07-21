b15c010f7f19daba4ec72e27c334777b292e0a6b
Mehdi Essalehi <mehdi.essalehi@gmail.com>
2023-07-21 11:32:41 +0100

    feat: added personal stats

--- src/stats/stats.controller.ts
+++ src/stats/stats.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Controller, Get, Req, UseGuards } from "@nestjs/common";
 import { StatsService } from "./stats.service";
 import { CookieAuthenticationGuard } from "src/auth/guards/cookie-authentication.guard";
+import { RequestWithUser } from "src/auth/request-with-user.interface";
 
 @Controller("stats")
 @UseGuards(CookieAuthenticationGuard)
 export class StatsController {
   constructor(private stats: StatsService) {}
 
+  /**
+   * Gets general stats for all members
+   */
   @Get()
-  async getStats() {
-    return await this.stats.getStats();
+  async getStats(@Req() request: RequestWithUser) {
+    const response = {};
+    if (request.user.privilege === "ADMIN") {
+      const globalStats = await this.stats.getStats();
+      response["globalStats"] = globalStats;
+    }
+    const personalStats = await this.stats.getPersonalStats(request.user);
+    response["personalStats"] = personalStats;
+    return response;
   }
 }
