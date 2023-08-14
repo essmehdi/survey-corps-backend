fb9a157fdd7713005089e058bbc40b6238e00626
Mehdi Essalehi <mehdi.essalehi@gmail.com>
2023-08-14 11:09:03 +0100

    fix: wrong dto for token

--- src/tokens/dto/token-with-user.dto.ts
+++ src/tokens/dto/token-with-user.dto.ts
@@ -1,8 +1,37 @@
 import { UserPublicDto } from "src/users/dto/user-public.dto";
 import { TokenDto } from "./token.dto";
-import { Type } from "class-transformer";
+import { Exclude, Expose, Type } from "class-transformer";
+import { Privilege, User } from "@prisma/client";
+import { ApiHideProperty } from "@nestjs/swagger";
+
+@Exclude()
+class TokenUser implements User {
+  @Expose()
+  id: number;
+
+  @Expose()
+  firstname: string;
+
+  @Expose()
+  lastname: string;
+
+  @Expose()
+  email: string;
+
+  @ApiHideProperty()
+  profilePicture: Buffer;
+
+  @ApiHideProperty()
+  password: string;
+
+  @ApiHideProperty()
+  privilege: Privilege;
+
+  @ApiHideProperty()
+  isActive: boolean;
+}
 
 export class TokenWithUserDto extends TokenDto {
-  @Type(() => UserPublicDto)
-  user?: UserPublicDto;
+  @Type(() => TokenUser)
+  user?: TokenUser;
 }
